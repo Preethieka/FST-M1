07688cc54e0f4c3e5177c6d305cab72d88fa2fe1
Preethieka <preethiekaa@gmail.com>
2024-12-30 13:35:17 +0530

    Add files via upload

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,26 @@
+package trainings;
+
+public class Activity2 {
+
+	public static void main(String[] args) {
+		int a[]= {10, 77, 10, 54, -11, 10};
+		int sum = 0;
+		for (int num : a) {
+			if(num == 10) {
+				sum += num;
+			}
+		}
+			
+		if(sum==30) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+	}
+
+
+
